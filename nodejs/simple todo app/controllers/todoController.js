df31bf266d560563f34da505db97720205e1649e
Trugamr <Trugamr@users.noreply.github.com>
2018-09-20 19:13:23 +0530

    mongodb connectivity

--- nodejs/simple todo app/controllers/todoController.js
+++ nodejs/simple todo app/controllers/todoController.js
@@ -1,25 +1,52 @@
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
-let data = [
-    { item: 'get milk' },
-    { item: 'play video games' },
-    { item: 'learn something new' }
-]
+//connect to database
+const dummyURL = 'MONGO_DB_URL';
+mongoose.connect(dummyURL)
+
+//create a schema - blueprint
+let todoSchema = new mongoose.Schema({ item: String });
+
+let Todo = mongoose.model('Todo', todoSchema);
+
+// let data = [
+//     { item: 'get milk' },
+//     { item: 'play video games' },
+//     { item: 'learn something new' }
+// ]
 
 const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 module.exports = function(app) {
     app.get('/todo', function(req, res) {
-        res.render('todo', { todos: data });
+        //get data from mongodb
+        Todo.find({}, function(err, data) {
+            if(err) throw err;
+            res.render('todo', { todos: data });
+        })        
+        // res.render('todo', { todos: data });
     })
 
     app.post('/todo', urlencodedParser, function(req, res) {
-        data.push(req.body);
-        res.json(data);
+        //get data from view and add to mongodb
+        let newTodo = Todo(req.body).save(function(err, data) {
+            if(err) throw err;
+            res.json(data);
+        })
+
+        // data.push(req.body);
+        // res.json(data);
     })
 
     app.delete('/todo/:item', function(req, res) {
-        data = data.filter(todo => todo.item.replace(/ /g, '-')!=req.params.item)
-        res.json(data);
+        //delete the requested itme from mongo db
+        Todo.find({item: req.params.item.replace(/\-/g, ' ')}).remove(function(err, data) {
+            if(err) throw err;
+            res.json(data);
+        })
+
+        // data = data.filter(todo => todo.item.replace(/ /g, '-')!=req.params.item)
+        // res.json(data);
     })
 }
